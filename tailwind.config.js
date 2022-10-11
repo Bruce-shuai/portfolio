7ca42eea9a4870bf6de7eef617d98b33edd61aa1
heyang <284970670@qq.com>
2022-10-11 20:32:47 +0800

    🤪

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      screens: {
+        "sm": "480px"
+      },
+      spacing: {
+        "big": "48rem"
+      }
+    },
+  },
+  plugins: [],
+}
